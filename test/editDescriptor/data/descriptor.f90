50019ca4ccf356f43e0a561d792a76283c5857da
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-14 08:52:15 +0900

    added unit test for data_edit_descriptor_type

---
+++ test/editDescriptor/data/descriptor.f90
@@ -0,0 +1,17 @@
+program test_data_descriptor
+    use, intrinsic :: iso_fortran_env
+    use :: fed_editDescriptor_data
+    use :: fassert
+    implicit none
+
+    call general_data_edit_descriptor_symbol_is_G()
+
+contains
+    subroutine general_data_edit_descriptor_symbol_is_G()
+        implicit none
+
+        ! test
+        call assert_equal(general_data_edit_descriptor_symbol, "G", &
+                          "general data edit descriptor symbol should be 'G'")
+    end subroutine general_data_edit_descriptor_symbol_is_G
+end program test_data_descriptor
