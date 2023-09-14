6be2e563f08a9bf06c3df5a820c48428f1dd5588
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-14 09:44:10 +0900

    added constructor for logical edit descriptor

--- src/fed.f90
+++ src/fed.f90
@@ -1,11 +1,13 @@
 module fed
     use :: fed_editDescriptor_data_char, only:char
+    use :: fed_editDescriptor_data_logical, only:logical
     use :: fed_format, only:format
     use :: fed_format_items, only:operator(//)
     implicit none
     private
     ! procedures
     public :: char
+    public :: logical
     public :: format
 
     ! operators
