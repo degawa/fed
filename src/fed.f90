df658cce2471e73948f90cac2d98c3d4cf620678
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-14 16:05:27 +0900

    updated fed module

--- src/fed.f90
+++ src/fed.f90
@@ -2,12 +2,14 @@ module fed
     use :: fed_editDescriptor_data_char, only:char
     use :: fed_editDescriptor_data_logical, only:logical
     use :: fed_editDescriptor_data_integer_decimal, only:int
+    use :: fed_editDescriptor_data_real_standard, only:real
     use :: fed_format, only:format
     use :: fed_format_items, only:operator(//)
     implicit none
     private
     ! procedures
     public :: int
+    public :: real
     public :: char
     public :: logical
     public :: format
