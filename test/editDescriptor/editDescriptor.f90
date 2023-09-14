c01f5002041fad1ee975de4b270dcfdd7f5b9415
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-14 09:16:59 +0900

    removed unnecessary variable in unit tests

--- test/editDescriptor/editDescriptor.f90
+++ test/editDescriptor/editDescriptor.f90
@@ -14,62 +14,45 @@ contains
     subroutine get_returns_zero_length_string_when_no_desc_is_set()
         implicit none
         type(edit_descriptor_type) :: desc
-        character(:), allocatable :: desc_str
 
         ! test
-        desc_str = desc%get()
-        call assert_equal(desc_str, "", &
+        call assert_equal(desc%get(), "", &
                           "get() should return zero-length string when no descriptor is set")
-
-        ! teardown
-        deallocate (desc_str)
     end subroutine get_returns_zero_length_string_when_no_desc_is_set
 
     subroutine get_returns_desc_when_desc_is_set()
         implicit none
         type(edit_descriptor_type) :: desc
-        character(:), allocatable :: desc_str
 
         ! setup
         call desc%set("descriptor")
 
         ! test
-        desc_str = desc%get()
-        call assert_equal(desc_str, "descriptor", &
+        call assert_equal(desc%get(), "descriptor", &
                           "get() should return descriptor as string when descriptor is set")
-
-        ! teardown
-        deallocate (desc_str)
     end subroutine get_returns_desc_when_desc_is_set
 
     subroutine destruct_deallocates_component_desc()
         implicit none
         type(edit_descriptor_type) :: desc
-        character(:), allocatable :: desc_str
 
         ! setup
         call desc%set("test_for_destruct")
-        desc_str = desc%get()
-        call assert_equal(desc_str, "test_for_destruct", &
+        call assert_equal(desc%get(), "test_for_destruct", &
                           "destract() should deallocate the component `desc`"// &
                           " [pre-test to confirm if the descriptor is set]")
 
         !test
         call desc%destruct()
 
-        desc_str = desc%get()
-        call assert_equal(desc_str, "", &
+        call assert_equal(desc%get(), "", &
                           "destract() should deallocate the component `desc`")
-
-        ! teardown
-        deallocate (desc_str)
     end subroutine destruct_deallocates_component_desc
 
     subroutine constructor_returns_edit_descriptor_type_instance()
         implicit none
         class(edit_descriptor_type), allocatable :: desc
         type(edit_descriptor_type) :: type_mold
-        character(:), allocatable :: desc_str
 
         ! test
         allocate (desc, source=edit_descriptor_type("test_for_constructor"))
@@ -77,13 +60,11 @@ contains
                          "constructor should return `edit_descriptor_type` instance"// &
                          " [type check]")
 
-        desc_str = desc%get()
-        call assert_equal(desc_str, "test_for_constructor", &
+        call assert_equal(desc%get(), "test_for_constructor", &
                           "constructor should return `edit_descriptor_type` instance"// &
                           " [component `desc` check]")
 
         ! teardown
         deallocate (desc)
-        deallocate (desc_str)
     end subroutine constructor_returns_edit_descriptor_type_instance
 end program test_editDescriptor
