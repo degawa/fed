75a30465383e3c2ca195be2d6389021714547923
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-14 18:57:34 +0900

    simplified retval name

--- src/editDescriptor/data/char/char.f90
+++ src/editDescriptor/data/char/char.f90
@@ -19,22 +19,22 @@ module fed_editDescriptor_data_char
 
 contains
     !>char_edit_descriptor_typeインスタンスを生成して返す．
-    pure function construct_char_edit_descriptor() result(new_char_edit_descriptor)
+    pure function construct_char_edit_descriptor() result(new_char_desc)
         implicit none
-        type(char_edit_descriptor_type) :: new_char_edit_descriptor
+        type(char_edit_descriptor_type) :: new_char_desc
             !! 生成されるchar_edit_descriptor_typeインスタンス
 
-        call new_char_edit_descriptor%set(char_edit_descriptor_symbol)
+        call new_char_desc%set(char_edit_descriptor_symbol)
     end function construct_char_edit_descriptor
 
     !>char_edit_descriptor_typeインスタンスを生成して返す．
     !>欄幅には`width`が用いられる．
-    pure function construct_char_edit_descriptor_w_width(width) result(new_char_edit_descriptor)
+    pure function construct_char_edit_descriptor_w_width(width) result(new_char_desc)
         use :: stdlib_strings
         implicit none
         integer(int32), intent(in) :: width
             !! 欄幅(>0)
-        type(char_edit_descriptor_type) :: new_char_edit_descriptor
+        type(char_edit_descriptor_type) :: new_char_desc
             !! 生成されるchar_edit_descriptor_typeインスタンス
 
         integer(int32) :: w
@@ -42,10 +42,10 @@ contains
 
         ! 欄幅が0以下の場合は欄幅のないA形編集記述子を返す
         if (w <= 0) then
-            new_char_edit_descriptor = char()
+            new_char_desc = char()
             return
         end if
 
-        call new_char_edit_descriptor%set(char_edit_descriptor_symbol//to_string(w))
+        call new_char_desc%set(char_edit_descriptor_symbol//to_string(w))
     end function construct_char_edit_descriptor_w_width
 end module fed_editDescriptor_data_char
