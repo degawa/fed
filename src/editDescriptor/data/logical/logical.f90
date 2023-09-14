2b36db47a0f013ebab348f32ecea907f9cadfc57
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-14 09:37:49 +0900

    added defined-type for logical edit descriptor

---
+++ src/editDescriptor/data/logical/logical.f90
@@ -0,0 +1,50 @@
+module fed_editDescriptor_data_logical
+    use, intrinsic :: iso_fortran_env
+    use :: fed_editDescriptor_data
+    implicit none
+    private
+    public :: logical
+
+    character(*), private, parameter :: logical_edit_descriptor_symbol = "L"
+        !! L形編集記述子（論理編集）に用いられる英字定数
+    integer(int32), private, parameter :: default_width = 1
+        !! L形編集記述子における欄の標準桁数
+
+    !>論理型編集記述子を取り扱う派生型．
+    type, public, extends(data_edit_descriptor_type) :: logical_edit_descriptor_type
+    end type logical_edit_descriptor_type
+
+    interface logical
+        procedure :: construct_logical_edit_descriptor_w_width
+        procedure :: construct_logical_edit_descriptor
+    end interface
+
+contains
+    !>logical_edit_descriptor_typeインスタンスを生成して返す．
+    !>欄幅には`width`が用いられる．
+    !>`width`が0以下の場合は，標準桁数が用いられる．
+    pure function construct_logical_edit_descriptor_w_width(width) result(new_logical_edit_descriptor)
+        use :: stdlib_strings
+        implicit none
+        integer(int32), intent(in) :: width
+            !! 欄幅(>0)
+        type(logical_edit_descriptor_type) :: new_logical_edit_descriptor
+            !! 生成されるlogical_edit_descriptor_typeインスタンス
+
+        integer(int32) :: w
+        w = width
+        if (w <= 0) w = default_width
+
+        call new_logical_edit_descriptor%set(logical_edit_descriptor_symbol//to_string(w))
+    end function construct_logical_edit_descriptor_w_width
+
+    !>logical_edit_descriptor_typeインスタンスを生成して返す．
+    !>欄幅には標準桁数が用いられる．
+    pure function construct_logical_edit_descriptor() result(new_logical_edit_descriptor)
+        implicit none
+        type(logical_edit_descriptor_type) :: new_logical_edit_descriptor
+            !! 生成されるlogical_edit_descriptor_typeインスタンス
+
+        new_logical_edit_descriptor = construct_logical_edit_descriptor_w_width(width=default_width)
+    end function construct_logical_edit_descriptor
+end module fed_editDescriptor_data_logical
