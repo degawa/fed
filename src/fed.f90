1ae8d5cfff09747e2748cd6c54dafa05cb87b8bb
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-14 22:23:51 +0900

    updated fed module

--- src/fed.f90
+++ src/fed.f90
@@ -4,6 +4,7 @@ module fed
     use :: fed_editDescriptor_data_integer_decimal, only:int
     use :: fed_editDescriptor_data_real_standard, only:real
     use :: fed_editDescriptor_data_complex_standard, only:complex
+    use :: fed_editDescriptor_characterString, only:str
     use :: fed_format, only:format
     use :: fed_format_items, only:operator(//)
     use :: fed_repeat, only:repeat
@@ -15,6 +16,7 @@ module fed
     public :: complex
     public :: char
     public :: logical
+    public :: str
     public :: format
     public :: repeat
 
