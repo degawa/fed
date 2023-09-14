4aac0cdeb30623f776a4406ce277f55ced5983d6
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-14 09:13:57 +0900

    added defined-type for character edit descriptor

---
+++ src/editDescriptor/data/char/char.f90
@@ -0,0 +1,51 @@
+module fed_editDescriptor_data_char
+    use, intrinsic :: iso_fortran_env
+    use :: fed_editDescriptor_data
+    implicit none
+    private
+    public :: char
+
+    character(*), private, parameter :: char_edit_descriptor_symbol = "A"
+        !! A形編集記述子に用いられる英字定数
+
+    !>文字型編集記述子を取り扱う派生型．
+    type, public, extends(data_edit_descriptor_type) :: char_edit_descriptor_type
+    end type char_edit_descriptor_type
+
+    interface char
+        procedure :: construct_char_edit_descriptor
+        procedure :: construct_char_edit_descriptor_w_width
+    end interface
+
+contains
+    !>char_edit_descriptor_typeインスタンスを生成して返す．
+    pure function construct_char_edit_descriptor() result(new_char_edit_descriptor)
+        implicit none
+        type(char_edit_descriptor_type) :: new_char_edit_descriptor
+            !! 生成されるchar_edit_descriptor_typeインスタンス
+
+        call new_char_edit_descriptor%set(char_edit_descriptor_symbol)
+    end function construct_char_edit_descriptor
+
+    !>char_edit_descriptor_typeインスタンスを生成して返す．
+    !>欄幅には`width`が用いられる．
+    pure function construct_char_edit_descriptor_w_width(width) result(new_char_edit_descriptor)
+        use :: stdlib_strings
+        implicit none
+        integer(int32), intent(in) :: width
+            !! 欄幅
+        type(char_edit_descriptor_type) :: new_char_edit_descriptor
+            !! 生成されるchar_edit_descriptor_typeインスタンス
+
+        integer(int32) :: w
+        w = width
+
+        ! 欄幅が0以下の場合は欄幅のないA形編集記述子を返す
+        if (w <= 0) then
+            new_char_edit_descriptor = char()
+            return
+        end if
+
+        call new_char_edit_descriptor%set(char_edit_descriptor_symbol//to_string(w))
+    end function construct_char_edit_descriptor_w_width
+end module fed_editDescriptor_data_char
