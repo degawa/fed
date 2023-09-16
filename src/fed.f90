d8a4a69b15cf3b545eca0143adad6e52bb979ac5
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-16 15:14:54 +0900

    updated fed module

--- src/fed.f90
+++ src/fed.f90
@@ -2,6 +2,10 @@ module fed
     use :: fed_editDescriptor_data_char, only:char
     use :: fed_editDescriptor_data_logical, only:logical
     use :: fed_editDescriptor_data_integer_decimal, only:int
+    use :: fed_editDescriptor_data_integer_binary, only:int_bin
+    use :: fed_editDescriptor_data_integer_octal, only:int_oct
+    use :: fed_editDescriptor_data_integer_hexadecimal, only:int_hex
+    use :: fed_editDescriptor_data_integer_facade, only:int, bin_digits, oct_digits, hex_digits
     use :: fed_editDescriptor_data_real_standard, only:real
     use :: fed_editDescriptor_data_complex_standard, only:complex
     use :: fed_editDescriptor_characterString, only:str
@@ -20,6 +24,12 @@ module fed
     public :: format
     public :: repeat
 
+    ! enumerators
+    public :: bin_digits, oct_digits, hex_digits
+
     ! operators
     public :: operator(//)
+
+    ! procedures opened to public for aliasing
+    public :: int_bin, int_oct, int_hex
 end module fed
