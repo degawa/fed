af1dd2f13e511e9d5b2aa47bbcd122ddbaf97fae
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-17 11:17:18 +0900

    updated an example

--- example/demo.f90
+++ example/demo.f90
@@ -17,7 +17,7 @@ program demo
     ! a format-specifier can be easily reuse by storing in a string
     stat = format("allocataion status: "//logical())
     memsize = format("memory size: "//int()//" bits")
-    fmt_bnd = format("array bounds = ["//repeat(int(4), rank(i))//"] x ["//repeat(int(4), rank(i))//"]")
+    fmt_bnd = format("array bounds = ["//2*int(4)//"] x ["//2*int(4)//"]")
 
     print stat, allocated(i) !allocataion status: T
     print memsize, storage_size(i)*size(i) !storage size: 2112 bits
