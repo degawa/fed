d6b343fe2e2a0abe259d4f861d4b736e6454a9fc
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-19 08:08:09 +0900

    fixed procedures name

--- src/format/format.f90
+++ src/format/format.f90
@@ -7,15 +7,15 @@ module fed_format
 
     !>書式仕様のコンストラクタ
     interface format
-        procedure :: construct_format_specifier
-        procedure :: construct_format_specifier_w_sep
-        procedure :: construct_format_specifier_by_descriptor
-        procedure :: construct_format_specifier_by_format_item
+        procedure :: construct_format_specification
+        procedure :: construct_format_specification_w_sep
+        procedure :: construct_format_specification_by_descriptor
+        procedure :: construct_format_specification_by_format_item
     end interface
 
 contains
     !>書式項目並びから書式仕様を文字列で生成して返す．
-    pure function construct_format_specifier(format_items) result(format_spec)
+    pure function construct_format_specification(format_items) result(format_spec)
         use :: strings_enclose
         implicit none
         type(format_items_type), intent(in) :: format_items
@@ -33,11 +33,11 @@ contains
         format_spec = format_spec//format_items%get_edit_descriptor_at(num_items)
 
         format_spec = enclose(format_spec, '(')
-    end function construct_format_specifier
+    end function construct_format_specification
 
     !>書式項目並びから書式仕様を文字列で生成して返す．
     !>書式項目は，区切り文字（`"`以外）で区切られる．
-    pure function construct_format_specifier_w_sep(format_items, separator) result(format_spec)
+    pure function construct_format_specification_w_sep(format_items, separator) result(format_spec)
         use :: strings_enclose
         implicit none
         type(format_items_type), intent(in) :: format_items
@@ -58,10 +58,10 @@ contains
         format_spec = format_spec//format_items%get_edit_descriptor_at(num_items)
 
         format_spec = enclose(format_spec, '(')
-    end function construct_format_specifier_w_sep
+    end function construct_format_specification_w_sep
 
     !>編集記述子から書式仕様を文字列で生成して返す．
-    function construct_format_specifier_by_descriptor(edit_descriptor) result(format_spec)
+    function construct_format_specification_by_descriptor(edit_descriptor) result(format_spec)
         use :: fed_editDescriptor
         implicit none
         class(edit_descriptor_type), intent(in) :: edit_descriptor
@@ -70,10 +70,10 @@ contains
             !! 書式仕様
 
         format_spec = format(items(edit_descriptor))
-    end function construct_format_specifier_by_descriptor
+    end function construct_format_specification_by_descriptor
 
     !>書式項目から書式仕様を文字列で生成して返す．
-    function construct_format_specifier_by_format_item(format_item) result(format_spec)
+    function construct_format_specification_by_format_item(format_item) result(format_spec)
         use :: fed_format_item
         implicit none
         class(format_item_type), intent(in) :: format_item
@@ -82,6 +82,6 @@ contains
             !! 書式仕様
 
         format_spec = format(items(format_item))
-    end function construct_format_specifier_by_format_item
+    end function construct_format_specification_by_format_item
 
 end module fed_format
