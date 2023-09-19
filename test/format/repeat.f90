46ab2409d9df10c6a36cf9329f31ed65b14637af
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-19 18:05:13 +0900

    updated unit test

--- test/format/repeat.f90
+++ test/format/repeat.f90
@@ -3,6 +3,9 @@ program test_repeat
     use :: fed_format_items
     use :: fed_format_item
     use :: fed_editDescriptor
+    use :: fed_editDescriptor_data
+    use :: fed_editDescriptor_control
+    use :: fed_editDescriptor_characterString
     use :: fassert
     implicit none
 
@@ -55,22 +58,100 @@ contains
         type(format_item_type) :: rep_item
 
         ! setup
-        itms = edit_descriptor_type("desc1")//edit_descriptor_type("desc2")
+        itms = dat("desc1")//dat("desc2")
         ! test
         rep_item = repeat(itms, ",", 2)
         call assert_equal(rep_item%get_edit_descriptor(), '2(desc1,",",desc2,",")', &
-                          "repeat(desc('desc1')//desc('desc2'), ',', 3) should return "//'2(desc1,",",desc2,",")')
+                          "repeat(dat('desc1')//dat('desc2'), ',', 3) should return "//'2(desc1,",",desc2,",")')
         ! teardown
         call itms%destruct()
         call rep_item%destruct()
 
         ! setup
-        itms = edit_descriptor_type("desc1")//edit_descriptor_type("desc2")//edit_descriptor_type("desc3")
+        itms = dat("desc1")//dat("desc2")//dat("desc3")
         ! test
         rep_item = repeat(itms, ",")
         call assert_equal(rep_item%get_edit_descriptor(), '*(desc1,",",desc2,",",desc3,",")', &
-                          "repeat(desc('desc1')//desc('desc2')//desc('desc3')) should return "// &
+                          "repeat(dat('desc1')//dat('desc2')//dat('desc3')) should return "// &
                           '*(desc1,",",desc2,",",desc3,",")')
+        ! teardown
+        call itms%destruct()
+        call rep_item%destruct()
+
+        ! setup
+        itms = dat("desc1")//str("desc2")
+        ! test
+        rep_item = repeat(itms, ",", 2)
+        call assert_equal(rep_item%get_edit_descriptor(), '2(desc1,"desc2",",")', &
+                          "repeat(dat('desc1')//str('desc2'), ',', 3) should return "//'2(desc1,"desc2",",")')
+        ! teardown
+        call itms%destruct()
+
+        ! setup
+        itms = dat("desc1")//ctrl("desc2")
+        ! test
+        rep_item = repeat(itms, ",", 2)
+        call assert_equal(rep_item%get_edit_descriptor(), '2(desc1,desc2,",")', &
+                          "repeat(dat('desc1')//ctrl('desc2'), ',', 3) should return "//'2(desc1,desc2,",")')
+        ! teardown
+        call itms%destruct()
+
+        ! setup
+        itms = str("desc1")//dat("desc2")
+        ! test
+        rep_item = repeat(itms, ",", 2)
+        call assert_equal(rep_item%get_edit_descriptor(), '2("desc1",",",desc2,",")', &
+                          "repeat(str('desc1')//dat('desc2'), ',', 3) should return "//'2("desc1",desc2,",")')
+        ! teardown
+        call itms%destruct()
+        call rep_item%destruct()
+
+        ! setup
+        itms = str("desc1")//str("desc2")
+        ! test
+        rep_item = repeat(itms, ",", 2)
+        call assert_equal(rep_item%get_edit_descriptor(), '2("desc1","desc2",",")', &
+                          "repeat(str('desc1')//str('desc2'), ',', 3) should return "//'2("desc1","desc2",",")')
+        ! teardown
+        call itms%destruct()
+        call rep_item%destruct()
+
+        ! setup
+        itms = str("desc1")//ctrl("desc2")
+        ! test
+        rep_item = repeat(itms, ",", 2)
+        call assert_equal(rep_item%get_edit_descriptor(), '2("desc1",desc2,",")', &
+                          "repeat(str('desc1')//ctrl('desc2'), ',', 3) should return "//'2("desc1",desc2,",")')
+        ! teardown
+        call itms%destruct()
+        call rep_item%destruct()
+
+        ! setup
+        itms = ctrl("desc1")//dat("desc2")
+        ! test
+        rep_item = repeat(itms, ",", 2)
+        call assert_equal(rep_item%get_edit_descriptor(), '2(desc1,",",desc2,",")', &
+                          "repeat(ctrl('desc1')//dat('desc2'), ',', 3) should return "//'2(desc1,",",desc2,",")')
+        ! teardown
+        call itms%destruct()
+        call rep_item%destruct()
+
+        ! setup
+        itms = ctrl("desc1")//str("desc2")
+        ! test
+        rep_item = repeat(itms, ",", 2)
+        call assert_equal(rep_item%get_edit_descriptor(), '2(desc1,"desc2",",")', &
+                          "repeat(ctrl('desc1')//str('desc2'), ',', 3) should return "//'2(desc1,"desc2",",")')
+        ! teardown
+        call itms%destruct()
+        call rep_item%destruct()
+
+        ! setup
+        itms = ctrl("desc1")//ctrl("desc2")
+        ! test
+        rep_item = repeat(itms, ",", 2)
+        call assert_equal(rep_item%get_edit_descriptor(), '2(desc1,desc2,",")', &
+                          "repeat(ctrl('desc1')//ctrl('desc2'), ',', 3) should return "//'2(desc1,desc2,",")')
         ! teardown
         call itms%destruct()
         call rep_item%destruct()
@@ -93,7 +174,7 @@ contains
         type(format_item_type) :: rep_item
 
         ! test
-        rep_item = repeat(edit_descriptor_type("desc1"), ",", 4)
+        rep_item = repeat(dat("desc1"), ",", 4)
         call assert_equal(rep_item%get_edit_descriptor(), '4(desc1,",")', &
                           "repeat(desc('desc1'), ',', 4) should return "//'4(desc1,",")')
         ! teardown
