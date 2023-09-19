15fb55899e282a538712b0d8ad3b83a5fb8a75d8
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-19 11:07:44 +0900

    added constructor for data edit descriptor

--- src/editDescriptor/data/descriptor.f90
+++ src/editDescriptor/data/descriptor.f90
@@ -3,6 +3,7 @@ module fed_editDescriptor_data
     use :: fed_editDescriptor
     implicit none
     private
+    public :: dat
 
     character(*), public, parameter :: general_data_edit_descriptor_symbol = "G"
         !! G形データ編集記述子（一般形編集）に用いられる英字定数
@@ -20,7 +21,33 @@ module fed_editDescriptor_data
         generic :: operator(*) => get_repeated_descriptor, r_get_repeated_descriptor
     end type data_edit_descriptor_type
 
+    !>ユーザ定義コンストラクタをビルトインコンストラクタと同じ名前
+    !>data_edit_descriptor_typeで呼べるようにするための総称名．
+    interface data_edit_descriptor_type
+        procedure :: construct_data_edit_descriptor
+    end interface
+
+    !>ユーザ定義コンストラクタをビルトインコンストラクタと同じ名前
+    !>datで呼べるようにするための総称名．
+    !>[[character_string_edit_descriptor_type]]がコンストラクタ
+    !>strを持つことに合わせるために定義されている．
+    interface dat
+        procedure :: construct_data_edit_descriptor
+    end interface
+
 contains
+    !>文字列として渡された編集記述子を用いて，
+    !>data_edit_descriptor_typeインスタンスを生成して返す．
+    pure function construct_data_edit_descriptor(descriptor) result(new_edit_descriptor)
+        implicit none
+        character(*), intent(in) :: descriptor
+            !! 編集記述子
+        type(data_edit_descriptor_type) :: new_edit_descriptor
+            !! 生成されるdata_edit_descriptor_typeインスタンス
+
+        call new_edit_descriptor%set(descriptor)
+    end function construct_data_edit_descriptor
+
     !>記述子反復数と結合されたデータ編集記述子を返す.
     !>この手続は，右側に記述子反復数がおかれる場合の乗算演算子`*`を
     !>オーバーロードする．
