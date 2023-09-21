c5d3a091efa1a7c0a803dfd29f29b0616734742d
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-21 14:48:03 +0900

    added block construct

--- example/control.f90
+++ example/control.f90
@@ -2,16 +2,18 @@ program ex_control
     use :: fed
     implicit none
 
-    logical :: l(2, 3)
+    block
+        logical :: l(2, 3)
 
-    print format(repeat(logical(), separator=",")), l
-    !F,F,T,T,F,F,
-    print format(repeat(logical()//terminate(), separator=",")), l
-    !F,F,T,T,F,F
-    print format("["//end_line()//repeat(logical(2), size(l))//end_line()//"]"), l
-    ![
-    ! F F T T F F
-    !]
+        print format(repeat(logical(), separator=",")), l
+        !F,F,T,T,F,F,
+        print format(repeat(logical()//terminate(), separator=",")), l
+        !F,F,T,T,F,F
+        print format("["//end_line()//repeat(logical(2), size(l))//end_line()//"]"), l
+        ![
+        ! F F T T F F
+        !]
+    end block
 
     print format(str("123456789012345"))
     print format("|"//move_to(8)//"^")
